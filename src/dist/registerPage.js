4a0241825198f67f6cf2f790de83bec918d1203a
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-02-15 15:36:51 +0100

    register handleSubmit function

--- src/dist/registerPage.js
+++ src/dist/registerPage.js
@@ -7,3 +7,21 @@ registerButton.addEventListener("click", function (e) {
     // location.href='index.html';
     console.table(signedUpUsers);
 });
+// Look at this function.. maybe you can use it to create new user
+function handleSubmit(e) {
+    e.preventDefault();
+    // e.stopPropagation();
+    var gender = this.elements.gender.value;
+    var firstName = this.elements.firstName.value;
+    var lastName = this.elements.lastName.value;
+    var password = this.elements.password.value;
+    var userName = this.elements.userName.value;
+    var email = this.elements.email.value;
+    var phone = this.elements.phone.value;
+    var location = this.elements.location.value;
+    var arr = [gender, firstName, lastName, password, userName, email, phone, location];
+    if (arr.some(function (ele) { return ele == ''; }))
+        return alert('missing field');
+    var newUser = new User(firstName, lastName, gender, userName, password, email, phone);
+    e.target.reset();
+}
--- src/registerPage.ts
+++ src/registerPage.ts
@@ -17,3 +17,31 @@ registerButton.addEventListener("click", (e) => {
     // location.href='index.html';
     console.table(signedUpUsers)
 });
+
+
+
+// Look at this function.. maybe you can use it to create new user
+function handleSubmit(e) {
+    e.preventDefault();
+    // e.stopPropagation();
+    const gender = this.elements.gender.value;
+    const firstName = this.elements.firstName.value;
+    const lastName = this.elements.lastName.value;
+    const password = this.elements.password.value;
+    const userName = this.elements.userName.value;
+    const email = this.elements.email.value;
+    const phone = this.elements.phone.value;
+    const location = this.elements.location.value;
+    const arr = [gender, firstName, lastName, password, userName, email, phone, location];
+    if(arr.some(ele => ele == '' )) return alert('missing field')
+    const newUser = new User(
+      firstName,
+      lastName,
+      gender,
+      userName,
+      password,
+      email,
+      phone
+    );
+    e.target.reset();
+  }
