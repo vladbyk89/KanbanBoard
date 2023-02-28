24b19fc4b1845ca3ccecbd02cd7424a1e81e25a7
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-02-28 10:56:23 +0100

    comment out itay code

--- src/newBoard.ts
+++ src/newBoard.ts
@@ -141,21 +141,21 @@ function createNewCard(cardName: string, list: Element) {
   ) as NodeListOf<HTMLDivElement>;
 }
 
-function allowDrop(ev)
-{
-ev.preventDefault();
-}
-function drag(ev)
-{
-ev.dataTransfer.setData("Text",ev.target.id);
-}
-function drop(ev)
-{
-ev.preventDefault();
-var data=ev.dataTransfer.getData("Text");
-var el = document.getElementById(data);
-el.parentNode.removeChild(el);
-}
+// function allowDrop(ev)
+// {
+// ev.preventDefault();
+// }
+// function drag(ev)
+// {
+// ev.dataTransfer.setData("Text",ev.target.id);
+// }
+// function drop(ev)
+// {
+// ev.preventDefault();
+// var data=ev.dataTransfer.getData("Text");
+// var el = document.getElementById(data);
+// el.parentNode.removeChild(el);
+// }
 
 
 
